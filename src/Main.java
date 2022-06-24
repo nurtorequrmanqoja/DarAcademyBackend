15493dd72e9ab171e2724316344177c30d0e4be2
nurtorequrmanqoja <nurtore.ad@gmail.com>
2022-06-24 10:43:06 +0600

    2 solution

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        ShuffleString shuffleString = new ShuffleString();
+        System.out.println(shuffleString.restoreString("llheo", new int[]{2,3,0,1,4}));
+
+        PalindromeNumber palindromeNumber = new PalindromeNumber();
+        System.out.println(palindromeNumber.isPalindrome(121));
+    }
+}
